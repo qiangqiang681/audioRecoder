f8b979a62aacba2f54b4cfcaa4847a2c7b56def7
qiangqiang681 <270976466@qq.com>
2019-05-15 15:50:44 +0800

    Update MainActivity.kt

--- app/src/main/java/com/plugin/eliv/audiorecoder/MainActivity.kt
+++ app/src/main/java/com/plugin/eliv/audiorecoder/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AudioEngine.getInstance().init(this).enableSystemAEC()
+        AudioEngine.getInstance().enableSystemAEC().init(this)
 
         recode.setOnClickListener {
             AudioEngine.getInstance().startRecord("[path]")
